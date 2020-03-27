bfb448c3b437b0f173712ee3c27d2332e71c6755
Luan Pinheiro <luan.gabriel.cardoso@hotmail.com>
2020-03-27 19:47:10 -0300

    atualizando

---
+++ comandos_gitHub.py
@@ -0,0 +1,18 @@
+"""
+Comandos GitHub
+
+Criar git repositorio:
+git init
+
+Mostrar historico do repositorio:
+git status
+
+Fazer um commit
+git add .
+git commit -m "First commit"
+git push origin master
+
+Add remote:
+git remote add origin endereco
+git remote add origin
+"""
